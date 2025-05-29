714eb792800c0a36d1061ee0998782f3598f43d4
Dani Sarfati <dani@funkervogt.com>
2025-05-29 13:51:06 -0400

    fixing pathing issues and memory issues for MSDOS

--- src/filecopy.cpp
+++ src/filecopy.cpp
@@ -2,65 +2,104 @@
 #include <fstream.h>
 #include <string.h>
 #include <time.h>
+#include <dos.h>
+#include <dir.h>
 #include "filecopy.h"
 #include "progress.h"
 #include "logger.h"
 
+// Normalize a file path to ensure it's valid for DOS
+void normalizePath(char* path) {
+    // Convert forward slashes to backslashes
+    for (int i = 0; path[i] != '\0'; i++) {
+        if (path[i] == '/') {
+            path[i] = '\\';
+        }
+    }
+}
+
 void FileCopy::copyFile(const char* sourcePath, const char* destPath) {
-    ifstream sourceFile(sourcePath, ios::binary);
-    ofstream destFile(destPath, ios::binary);
+    char normalizedSource[256];
+    char normalizedDest[256];
+    
+    // Make copies so we can safely modify them
+    strcpy(normalizedSource, sourcePath);
+    strcpy(normalizedDest, destPath);
+    
+    // Convert slashes for DOS compatibility
+    normalizePath(normalizedSource);
+    normalizePath(normalizedDest);
+    
+    // Open files with binary mode for proper handling
+    ifstream sourceFile(normalizedSource, ios::binary);
+    ofstream destFile(normalizedDest, ios::binary);
 
     if (!sourceFile) {
-        cerr << "Error opening source file: " << sourcePath << endl;
+        cerr << "Error opening source file: " << normalizedSource << endl;
         return;
     }
     if (!destFile) {
-        cerr << "Error opening destination file: " << destPath << endl;
+        cerr << "Error opening destination file: " << normalizedDest << endl;
         return;
     }
 
+    // Get file size
     sourceFile.seekg(0, ios::end);
     long fileSize = sourceFile.tellg();
     sourceFile.seekg(0, ios::beg);
 
-    char buffer[4096];
+    // Use a modest buffer size - 8KB is good for DOS
+    const int BUFFER_SIZE = 8192;
+    char buffer[BUFFER_SIZE];
     long bytesRead;
     time_t startTime = time(NULL);
     long totalBytesCopied = 0;
     double maxSpeed = 0.0, minSpeed = 0.0, totalSpeed = 0.0;
     int speedCount = 0;
+    time_t lastUpdateTime = startTime;
+    time_t currentTime;
 
     Progress progress;
     progress.showProgressBar(0, fileSize, 0.0);
 
-    while (sourceFile) {
-        sourceFile.read(buffer, sizeof(buffer));
+    // Process file in manageable chunks
+    while (sourceFile && !sourceFile.eof() && totalBytesCopied < fileSize) {
+        sourceFile.read(buffer, BUFFER_SIZE);
         bytesRead = sourceFile.gcount();
+        
         if (bytesRead <= 0) break;
         
         destFile.write(buffer, bytesRead);
         totalBytesCopied += bytesRead;
 
-        time_t currentTime = time(NULL);
-        double duration = difftime(currentTime, startTime);
-        double currentSpeed = (duration > 0) ? (totalBytesCopied / duration) : 0.0;
+        // Update speed calculation less frequently to avoid console flicker
+        currentTime = time(NULL);
+        if (difftime(currentTime, lastUpdateTime) >= 1.0) {
+            double duration = difftime(currentTime, startTime);
+            double currentSpeed = (duration > 0) ? (totalBytesCopied / duration) : 0.0;
 
-        if (speedCount == 0) {
-            maxSpeed = minSpeed = currentSpeed;
-        } else {
-            if (currentSpeed > maxSpeed) maxSpeed = currentSpeed;
-            if (currentSpeed < minSpeed) minSpeed = currentSpeed;
+            if (speedCount == 0) {
+                maxSpeed = minSpeed = currentSpeed;
+            } else {
+                if (currentSpeed > maxSpeed) maxSpeed = currentSpeed;
+                if (currentSpeed < minSpeed && currentSpeed > 0) minSpeed = currentSpeed;
+            }
+
+            totalSpeed += currentSpeed;
+            speedCount++;
+
+            progress.showProgressBar(totalBytesCopied, fileSize, currentSpeed);
+            lastUpdateTime = currentTime;
         }
-
-        totalSpeed += currentSpeed;
-        speedCount++;
-
-        progress.showProgressBar(totalBytesCopied, fileSize, currentSpeed);
     }
 
+    // Final update
     time_t endTime = time(NULL);
     double totalDuration = difftime(endTime, startTime);
-    double averageSpeed = (speedCount > 0) ? (totalSpeed / speedCount) : 0.0;
+    double averageSpeed = (totalDuration > 0) ? (totalBytesCopied / totalDuration) : 0.0;
+
+    // Show 100% completion
+    progress.showProgressBar(fileSize, fileSize, averageSpeed);
 
     Logger logger;
     logger.logTransferDetails(sourcePath, destPath, fileSize, maxSpeed, minSpeed, averageSpeed, totalDuration);
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,22 @@
 #include <iostream.h>
 #include <string.h>
+#include <dos.h>
+#include <dir.h>
 #include "filecopy.h"
 
 #define VERSION "0.1"
 
 void showUsage(const char* programName) {
-    cout << "FileCopy Utility v" << VERSION << endl;
-    cout << "GitHub: https://github.com/danifunker/dos-file-test" << endl;
-    cout << endl;
     cout << "Usage: " << programName << " <source_file> <destination_file>" << endl;
     cout << endl;
     cout << "Parameters:" << endl;
     cout << "  <source_file>      - Path to the file to be copied" << endl;
-    cout << "  <destination_file> - Path where the file will be copied to" << endl;
+    cout << "  <destination_folder> - Path where the file will be copied to" << endl;
     cout << endl;
-    cout << "Example: " << programName << " C:\\DATA.TXT D:\\BACKUP.TXT" << endl;
+    cout << "Examples: " << endl;
+    cout << "  " << programName << " C:\\DATA.TXT D:\\BACKUP.TXT" << endl;
+    cout << "  " << programName << " DATA.TXT BACKUP.TXT" << endl;
+    cout << "  " << programName << " ..\\SOURCE\\DATA.TXT ..\\DEST\\DATA.TXT" << endl;
 }
 
 int main(int argc, char* argv[]) {
@@ -22,8 +24,8 @@ int main(int argc, char* argv[]) {
     cout << "GitHub: https://github.com/danifunker/dos-file-test" << endl;
     cout << endl;
 
-    char sourcePath[256] = {0};
-    char destinationPath[256] = {0};
+    char sourcePath[MAXPATH] = {0};
+    char destinationPath[MAXPATH] = {0};
 
     // Process command line arguments
     if (argc < 3) {
@@ -32,8 +34,25 @@ int main(int argc, char* argv[]) {
     }
     
     // Copy arguments to our buffers
-    strncpy(sourcePath, argv[1], 255);
-    strncpy(destinationPath, argv[2], 255);
+    strcpy(sourcePath, argv[1]);
+    strcpy(destinationPath, argv[2]);
+    
+    // Get absolute paths if needed
+    if (sourcePath[0] != '\\' && sourcePath[1] != ':') {
+        char temp[MAXPATH];
+        getcwd(temp, MAXPATH);
+        strcat(temp, "\\");
+        strcat(temp, sourcePath);
+        strcpy(sourcePath, temp);
+    }
+    
+    if (destinationPath[0] != '\\' && destinationPath[1] != ':') {
+        char temp[MAXPATH];
+        getcwd(temp, MAXPATH);
+        strcat(temp, "\\");
+        strcat(temp, destinationPath);
+        strcpy(destinationPath, temp);
+    }
     
     cout << "Source: " << sourcePath << endl;
     cout << "Destination: " << destinationPath << endl;
