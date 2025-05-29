f9d0bda9cd0e9176bf76a3925bd2895ac5602cab
Dani Sarfati <dani@funkervogt.com>
2025-05-29 13:42:05 -0400

    moved makefile into src folder and added usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,43 @@
-#include <iostream.h>  // Note: no .h for modern headers, but .h for old-style headers
+#include <iostream.h>
 #include <string.h>
 #include "filecopy.h"
 
-int main() {
-    char sourcePath[256];    // Use fixed-size character arrays instead of std::string
-    char destinationPath[256];
+#define VERSION "0.1"
 
-    cout << "Enter the source file path: ";
-    cin.getline(sourcePath, 256);  // Use C-style getline
+void showUsage(const char* programName) {
+    cout << "FileCopy Utility v" << VERSION << endl;
+    cout << "GitHub: https://github.com/danifunker/dos-file-test" << endl;
+    cout << endl;
+    cout << "Usage: " << programName << " <source_file> <destination_file>" << endl;
+    cout << endl;
+    cout << "Parameters:" << endl;
+    cout << "  <source_file>      - Path to the file to be copied" << endl;
+    cout << "  <destination_file> - Path where the file will be copied to" << endl;
+    cout << endl;
+    cout << "Example: " << programName << " C:\\DATA.TXT D:\\BACKUP.TXT" << endl;
+}
+
+int main(int argc, char* argv[]) {
+    cout << "FileCopy Utility v" << VERSION << endl;
+    cout << "GitHub: https://github.com/danifunker/dos-file-test" << endl;
+    cout << endl;
+
+    char sourcePath[256] = {0};
+    char destinationPath[256] = {0};
+
+    // Process command line arguments
+    if (argc < 3) {
+        showUsage(argv[0]);
+        return 1;
+    }
     
-    cout << "Enter the destination file path: ";
-    cin.getline(destinationPath, 256);
+    // Copy arguments to our buffers
+    strncpy(sourcePath, argv[1], 255);
+    strncpy(destinationPath, argv[2], 255);
+    
+    cout << "Source: " << sourcePath << endl;
+    cout << "Destination: " << destinationPath << endl;
+    cout << endl;
 
     FileCopy fileCopy;
     fileCopy.copyFile(sourcePath, destinationPath);
